ed76ea7d09d8eb9cffa98226f7b506d5ae9ebcad
Bilal A <bila0765@stud.kea.dk>
2021-03-11 09:18:15 +0100

    First commit

---
+++ src/test/java/com/bibliotek/bibliotek/BibliotekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotek.bibliotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
