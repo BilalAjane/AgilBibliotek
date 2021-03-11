ff240fb2b9fd2b9083f47742b8f03d285c25ec93
MatiasStorm <mat-435@hotmail.com>
2021-03-11 10:01:39 +0100

    Add getters and setters to Media and Book

--- src/main/java/com/bibliotek/bibliotek/model/Book.java
+++ src/main/java/com/bibliotek/bibliotek/model/Book.java
@@ -5,6 +5,27 @@ public class Book {
     private String title;
     private String isbn;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
